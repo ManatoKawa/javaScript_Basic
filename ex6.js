3b1874f0a92ad024541d173001e614bd5927f94e
ManatoKawa <2004240008km@edu.oca.ac.jp>
2023-01-28 16:11:44 +0900

    gggg

---
+++ ex6.js
@@ -0,0 +1,22 @@
+{"id":0,"name":"My NAme","own_car":true}
+info = {"id": 0, "name": "My Name", "own_car": true};
+info.id;
+info["name"];
+info.id = 5;
+info["own_car"] = false;
+info.age = 18
+info
+"age" in info;
+"parent" in info;
+delete info.age;
+info
+info = {"id": 0, "name": "My Name", "own_car": true};
+test = {"id": 1, "name": "My Name", "own_car": true,age:40};
+info = {"id": 0, "name": "My Name", "own_car": true,age:20};
+test = {"id": 1, "name": "My Name", "own_car": true,age:40};
+info = {"id": 0, "name": "My test", "own_test": true,age:30};
+new_test2 = {...info,...test};
+const PI = 3.14 
+pi = 3.14 
+pi2 = (pi=3.15)
+pi2
